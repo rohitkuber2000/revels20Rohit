f1d90124dec94b698217cda27d05c2327cfa723f
rohitkuber2000 <rohitkuber42@gmail.com>
2019-12-07 13:46:36 +0530

    Performed the given tasks

--- revels20Rohit/ViewController.swift
+++ revels20Rohit/ViewController.swift
@@ -13,7 +13,66 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        evenn()
+        fibonaccis(10)
+        
+        lstring("Rohit", "John is a good boy")
+        let names = ["Rohit","Naman","Akhilesh","Tushar"]
+         
+        strswap(names,2)
+
     }
+    //Function to print even numbers
+    func evenn(){
+        for i in 1...40{
+            if(i%2==0){
+            print(i)
+        }
+    }
+    }
+//Function to print Fibonacci numbers
+    func fibonaccis(_ num :Int){
+        var first = 0
+        var second  = 1
+        var i = 2
+        var sum = 0
+        print(first)
+        print(second)
+        while (i<num){
+            sum = first + second
+            print(sum)
+            first = second
+            second = sum
+            i = i + 1
+        }
+    }
+   
+    //Function to print biggest of 2 strings
+    func lstring(_ str1: String, _ str2: String){
+        let l1 = str1.count
+        let l2 = str2.count
+        if(l1 >= l2){
+            print(str1)
+    }
+        else{
+            print(str2)
+    }
+    }
+    //Function to swap String array elements
+    func strswap(_ strarr:[String], _ ind: Int){
+     var arr = strarr
+        if(ind<strarr.count){
+            arr.swapAt(0,ind)
+            for word in arr{
+                print(word)
+            }
+        }
+        else{
+            print("Invalid index")
+        }
+        
+    }
+    
 
 
 }
